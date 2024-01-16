c615c89c630eff916a3d95fd9cadfc225cc08c28
Maleja2024 <mayramirez_21@hotmail.com>
2024-01-16 11:11:31 -0500

    commit 1 para el reto

---
+++ src/main/java/com/defaultpackage/questions/DefaultQuestion.java
@@ -0,0 +1,16 @@
+package com.defaultpackage.questions;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+
+
+import sun.misc.Queue;
+
+public class DefaultQuestion implements Question<String>{
+
+
+    @Override
+    public String answeredBy(Actor actor) {
+        return null;
+    }
+}
